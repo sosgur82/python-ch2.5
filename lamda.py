1d3961fb10d7f6d3c6bf14829d7d9394b7aeb2c2
sosgur82 <sosgur82@gamil.com>
2017-10-10 14:15:40 +0900

    sort key 문제 수정

--- lamda.py
+++ lamda.py
@@ -1,23 +1,24 @@
 # lamda 함수
 
+
 def blahblah(x):
     return x**2
 
+
 for i in range(10):
-    print("{0}:{1}".format(i,blahblah(i)), end=' ')
+    print("{0}:{1}".format(i, blahblah(i)), end=' ')
 else:
     print()
 
-
 for i in range(10):
-    print("{0}:{1}".format(i, (lambda x:x**2)(i)), end=' ')
+    print("{0}:{1}".format(i, (lambda x: x**2)(i)), end=' ')
 else:
     print()
 
-strings = ['foo', 'a', 'card', 'bar', 'abab', 'bar', 'aaa', 'a']
-
-strings.sort(key= lambda x: len(x))
+strings = ['foo', 'card', 'bar', 'abab', 'aaaa', 'abab', 'foo']
+strings.sort(key=lambda x: len(x), reverse=False)
 print(strings)
 
-strings.sort(key= lambda x: strings.count(x))
+strings = sorted(strings, key=lambda x: strings.count(x))
+#strings.sort(key=lambda x: k(x, strings), reverse=False)
 print(strings)
